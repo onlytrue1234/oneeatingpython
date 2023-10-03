0f1dd1dba86d8c8853bd533edc2bc2b08bc33075
onlytrue1234 <146081797+onlytrue1234@users.noreply.github.com>
2023-10-03 16:16:01 +0900

    Add files via upload

---
+++ ibsarulattuckhasiva.py
@@ -0,0 +1,2 @@
+age = int(input('나이를 입력해주세요: '))
+print(age - 3, '살로 보여요')
---
+++ zipgagosipda.py
@@ -0,0 +1,8 @@
+count = 0
+while count < 3:
+    count = count + 1
+    if count == 2:
+        break
+    print(count)
+    
+    
